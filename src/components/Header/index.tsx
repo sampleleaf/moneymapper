139d86e74d035911301e073c3010a1c47f127362
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-04-09 15:55:15 +0800

    feat(UI): Complete mobile UI header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
+import header from '@/css/Header.module.css'
+
 const Header: React.FC = () => {
     return (
-        <div>
-            <div><i className="fa-solid fa-bars"></i></div>
+        <div className={header.container}>
+            <div className={header.bar}><i className="fa-solid fa-bars"></i></div>
+            <div className={header.title}>MoneyMapper</div>
+            <div className={header.iconContainer}>
+                <i className="fa-solid fa-map-location-dot"></i>
+                <i className="fa-solid fa-circle-user"></i>
+            </div>
         </div>
     )
 }
