d90b35e0e712f5ea0a04ed497c7017d95dc8893d
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-04-10 14:26:43 +0800

    feat: Install the SDK and initialize Firebase

---
+++ src/utils/firebase.tsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIRE_apiKey,
+  authDomain: import.meta.env.VITE_FIRE_authDomain,
+  projectId: import.meta.env.VITE_FIRE_projectId,
+  storageBucket: import.meta.env.VITE_FIRE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FIRE_messagingSenderId,
+  appId: import.meta.env.VITE_FIRE_appId,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_FIRE_apiKey: string
+    readonly VITE_FIRE_authDomain: string
+    readonly VITE_FIRE_projectId: string
+    readonly VITE_FIRE_storageBucket: string
+    readonly VITE_FIRE_messagingSenderId: string
+    readonly VITE_FIRE_appId: string
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
