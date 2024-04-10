6f645bfc000ae9fb3ce49a0ab96adf5656b44099
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-04-10 19:45:18 +0800

    feat: basic interface of homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import "@/App.css";
 import Header from "@/components/Header";
 import Login from "@/pages/Login";
+import Home from "@/pages/Home";
 import { useState } from "react";
 
 const App: React.FC = () => {
-  const [login, setLogin] = useState<string | null>(localStorage.getItem("loginData"));
-  // const isLogin = localStorage.getItem("loginData");
-  return <>{login ? <Header setLogin={setLogin} /> : <Login setLogin={setLogin} />}</>;
-}
+  const [login, setLogin] = useState<string | null>(
+    localStorage.getItem("loginData")
+  );
+  
+  return (
+    <>
+      {login ? <Header setLogin={setLogin} /> : <Login setLogin={setLogin} />}
+      <Home />
+    </>
+  );
+};
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,87 @@
+import home from "@/css/Home.module.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className={home.container}>
+        <div className={home.header}>2024年4月</div>
+        <div className={home.analyze}>
+          <div>
+            <p>月支出</p>
+            <p>$99999</p>
+          </div>
+          <div>
+            <p>月收入</p>
+            <p>$99999</p>
+          </div>
+        </div>
+        <div className={home.echart}>
+          <div className={home.remainderSpace}></div>
+          <div className={home.monthRemainder}>
+            <p>月結餘</p>
+            <p>$999999</p>
+          </div>
+        </div>
+        <div className={home.itemsByDayThisMonth}>
+          <div>
+            <div className={home.dateAndRemainder}>
+              <div className={home.date}>
+                <p>2024年12月28日</p>
+                <p>星期幾</p>
+              </div>
+              <p className={home.dayRemainder}>$-48</p>
+            </div>
+            <div className={home.line}></div>
+            <div className={home.itemsByDay}>
+              <div className={home.items}>
+                <div className={home.item}>
+                  <p>圖</p>
+                  <p>圖標預設名稱或自訂備註</p>
+                </div>
+                <p>$-199999</p>
+              </div>
+              <div className={home.items}>
+                <div className={home.item}>
+                  <p>圖</p>
+                  <p>圖標預設名稱或自訂備註</p>
+                </div>
+                <p>$-199999</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className={home.dateAndRemainder}>
+              <div className={home.date}>
+                <p>2024年12月28日</p>
+                <p>星期幾</p>
+              </div>
+              <p className={home.dayRemainder}>$-48</p>
+            </div>
+            <div className={home.line}></div>
+            <div className={home.itemsByDay}>
+              <div className={home.items}>
+                <div className={home.item}>
+                  <p>圖</p>
+                  <p>圖標預設名稱或自訂備註</p>
+                </div>
+                <p>$-199999</p>
+              </div>
+              <div className={home.items}>
+                <div className={home.item}>
+                  <p>圖</p>
+                  <p>圖標預設名稱或自訂備註</p>
+                </div>
+                <p>$-199999</p>
+              </div>
+            </div>
+          </div>
+          <a className={home.addItem} href="./add">
+            <i className="fa-solid fa-circle-xmark"></i>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
