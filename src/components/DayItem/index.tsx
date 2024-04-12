d2cad93284f8a4f17275fa9640a9fe0f0ad53c28
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-04-12 13:15:13 +0800

    feat: add a DayItem component

--- src/components/DayItem/index.tsx
+++ src/components/DayItem/index.tsx
@@ -1,7 +1,80 @@
-const DayItem = () => {
-    return(
-        <>hey</>
-    )
-}
+import home from '@/css/Home.module.css'
+import { db } from "@/utils/firebase";
+import { doc, getDoc, updateDoc, arrayRemove } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-export default DayItem
+const DayItem: React.FC<{day: string}> = ({day}) => {
+    const [items, setItems] = useState<
+    {
+      id: string;
+      item: string;
+      price: number;
+    }[]
+  >([]);
+
+  const [itemRemoved, setItemRemoved] = useState<boolean>(false)
+
+  useEffect(() => {
+    const response = localStorage.getItem("loginData");
+    if (response !== null) {
+      const data = JSON.parse(response);
+      (async () => {
+        const docRef = doc(db, "users", data.id, "2024", "4");
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+        //   console.log("Document data:", docSnap.data());
+          setItems(docSnap.data()[day]);
+        } else {
+          // docSnap.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })();
+    }
+    return () => setItemRemoved(false)
+
+  }, [itemRemoved]);
+
+  const handleItemRemove = async (id: object, day: string) => {
+    const response = localStorage.getItem("loginData");
+    if (response !== null) {
+      const data = JSON.parse(response);
+      const docRef = doc(db, "users", data.id, "2024", "4");
+      await updateDoc(docRef, {       
+        [day]: arrayRemove(id)
+      });
+      setItemRemoved(true)
+    }
+  };
+  return (
+    <div>
+      <div className={home.dateAndRemainder}>
+        <div className={home.date}>
+          <p>2024年4月{day}日</p>
+        </div>
+        <p className={home.dayRemainder}>
+          ${items && items.reduce((acc, cur) => acc + cur.price, 0)}
+        </p>
+      </div>
+      <div className={home.line}></div>
+      <div className={home.itemsByDay}>
+        {items &&
+          items.map((item) => (
+            <div key={item.id} className={home.items}>
+              <div className={home.item}>
+                <p>{item.item}</p>
+              </div>
+              <p>${item.price}</p>
+              <div
+                onClick={() => handleItemRemove(item, day)}
+                className={home.trash}
+              >
+                <i className="fa-regular fa-trash-can"></i>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default DayItem;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,11 @@
 import home from "@/css/Home.module.css";
+import DayItem from "@/components/DayItem";
+import { useEffect, useState } from "react";
 import { db } from "@/utils/firebase";
 import { doc, getDoc, updateDoc, arrayRemove } from "firebase/firestore";
-import { useEffect, useState } from "react";
 
 const Home: React.FC = () => {
-  const [items, setItems] = useState<
-    {
-      id: string;
-      item: string;
-      price: number;
-    }[]
-  >([]);
-
-  const [itemRemoved, setItemRemoved] = useState<boolean>(false)
+  const [days, setDays] = useState<string[]>([]);
 
   useEffect(() => {
     const response = localStorage.getItem("loginData");
@@ -22,29 +15,15 @@ const Home: React.FC = () => {
         const docRef = doc(db, "users", data.id, "2024", "4");
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
-          console.log("Document data:", docSnap.data());
-          setItems(docSnap.data()[11]);
+          // console.log(Object.keys(docSnap.data()));
+          setDays(Object.keys(docSnap.data()));
         } else {
           // docSnap.data() will be undefined in this case
           console.log("No such document!");
         }
       })();
     }
-    return () => setItemRemoved(false)
-
-  }, [itemRemoved]);
-
-  const handleItemRemove = async (id: object) => {
-    const response = localStorage.getItem("loginData");
-    if (response !== null) {
-      const data = JSON.parse(response);
-      const docRef = doc(db, "users", data.id, "2024", "4");
-      await updateDoc(docRef, {       
-        11: arrayRemove(id)
-      });
-      setItemRemoved(true)
-    }
-  };
+  }, []);
 
   return (
     <>
@@ -68,57 +47,7 @@ const Home: React.FC = () => {
           </div>
         </div>
         <div className={home.itemsByDayThisMonth}>
-          <div>
-            <div className={home.dateAndRemainder}>
-              <div className={home.date}>
-                <p>2024年4月11日</p>
-              </div>
-              <p className={home.dayRemainder}>
-                ${items && items.reduce((acc, cur) => acc + cur.price, 0)}
-              </p>
-            </div>
-            <div className={home.line}></div>
-            <div className={home.itemsByDay}>
-              {items &&
-                items.map((item) => (
-                  <div key={item.id} className={home.items}>
-                    <div className={home.item}>
-                      <p>{item.item}</p>
-                    </div>
-                    <p>${item.price}</p>
-                    <div onClick={() => handleItemRemove(item)} className={home.trash}>
-                      <i className="fa-regular fa-trash-can"></i>
-                    </div>
-                  </div>
-                ))}
-            </div>
-          </div>
-          <div>
-            <div className={home.dateAndRemainder}>
-              <div className={home.date}>
-                <p>2024年12月28日</p>
-                <p>星期幾</p>
-              </div>
-              <p className={home.dayRemainder}>$-48</p>
-            </div>
-            <div className={home.line}></div>
-            <div className={home.itemsByDay}>
-              <div className={home.items}>
-                <div className={home.item}>
-                  <p>圖</p>
-                  <p>圖標預設名稱或自訂備註</p>
-                </div>
-                <p>$-199999</p>
-              </div>
-              <div className={home.items}>
-                <div className={home.item}>
-                  <p>圖</p>
-                  <p>圖標預設名稱或自訂備註</p>
-                </div>
-                <p>$-199999</p>
-              </div>
-            </div>
-          </div>
+          {days && days.map((day) => <DayItem key={day} day={day} />)}
           <a className={home.addItem} href="./create">
             <i className="fa-solid fa-circle-xmark"></i>
           </a>
