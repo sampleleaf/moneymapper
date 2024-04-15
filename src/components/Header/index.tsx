baf83359220375956624d93ec205e6269193e93c
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-04-15 10:05:49 +0800

    feat: header sidebar basic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,46 @@
 import header from "@/css/Header.module.css";
+import { useState } from "react";
 
 const Header: React.FC<{ setLogin: Function }> = ({ setLogin }) => {
+  const [sidebar, setSidebar] = useState(false);
+
   const handleLogout = () => {
     localStorage.removeItem("loginData");
     setLogin(localStorage.getItem("loginData"));
   };
 
+  const handleSidebar = () => {
+    setSidebar(!sidebar);
+  };
+
   return (
-    <div className={header.container}>
-      <div className={header.bar}>
-        <i className="fa-solid fa-bars"></i>
+    <>
+      <div
+        onClick={handleSidebar}
+        className={header.sidebarBackground}
+        style={sidebar ? { backgroundColor: 'rgba(0, 0, 0, 0.5)', pointerEvents: "auto" } : {}}
+      >
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className={header.sidebar}
+          style={sidebar ? { transform: "translateX(0)" } : {}}
+        ></div>
       </div>
-      <a href="/"><div className={header.title}>MoneyMapper</div></a>
-      <div className={header.iconContainer}>
-        <i className="fa-solid fa-map-location-dot"></i>
-        <p onClick={handleLogout} className={header.logout}>
-          登出
-        </p>
+      <div className={header.container}>
+        <div onClick={handleSidebar} className={header.bar}>
+          <i className="fa-solid fa-bars"></i>
+        </div>
+        <a href="/">
+          <div className={header.title}>MoneyMapper</div>
+        </a>
+        <div className={header.iconContainer}>
+          <i className="fa-solid fa-map-location-dot"></i>
+          <p onClick={handleLogout} className={header.logout}>
+            登出
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
