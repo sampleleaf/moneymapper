d76c2b5ff22465319dfe42bda6e86fabbd13c669
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-04-16 16:57:51 +0800

    test: success to use useOutletContext

--- src/components/DetailGroup/Pay.tsx
+++ src/components/DetailGroup/Pay.tsx
@@ -1,31 +1,41 @@
 import { Chart } from "react-google-charts";
+import { useOutletContext } from "react-router-dom";
+import pay from "@/css/Pay.module.css";
 
 export const data = [
-  ["Task", "Hours per Day"],
+  ["Major", "Degrees"],
   ["Work", 5],
   ["Eat", 2],
   ["Commute", 2],
   ["Watch TV", 8],
-  ["Sleep", 7], 
+  ["Sleep", 7],
 ];
 
 export const options = {
-  pieHole: 0.4,
+  pieHole: 0.5,
   is3D: false,
-  legend: 'none',
+  legend: "none",
 };
 
+type ContextType = { years: number };
+
 const Pay = () => {
+
+  const {years} = useOutletContext<ContextType>()
+
   return (
-    <div className="hahaha">
-      <Chart
-        chartType="PieChart"
-        width="100%"
-        height="400px"
-        data={data}
-        options={options}
-      />
-    </div>
+    <>
+      <div className={pay.googleChart}>
+        <Chart
+          chartType="PieChart"
+          width="100%"
+          height="400px"
+          data={data}
+          options={options}
+        />
+      </div>
+      <div>{years}</div>
+    </>
   );
 };
 
--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,23 +1,95 @@
-import details from '@/css/Details.module.css'
-import { Link, Outlet } from 'react-router-dom'
+import details from "@/css/Details.module.css";
+import { Link, Outlet, useOutletContext } from "react-router-dom";
+import home from "@/css/Home.module.css";
+import { useState } from "react";
 
-const Details: React.FC<{ detailsTranslateX: string, setDetailsTranslateX: Function }> = ({ detailsTranslateX, setDetailsTranslateX }) => {
+type ContextType = { years: number };
 
-    return(
-        <>  
-            <div className={details.space}></div>
-            <Link to=".." className={details.back}><i className="fa-solid fa-chevron-left"></i></Link>
-            <div className={details.navbarLayout}>
-                <div className={details.triggerLink} style={{transform : detailsTranslateX}}></div>
-                <div className={details.navbarContainer}>
-                    <Link onClick={() => setDetailsTranslateX('translateX(-102.5%)')} to="pay">支出</Link>
-                    <Link onClick={() => setDetailsTranslateX('translateX(0)')} to="income">收入</Link>
-                    <Link onClick={() => setDetailsTranslateX('translateX(102.5%)')} to="remainder">結餘</Link>
-                </div>
+const Details: React.FC<{
+  detailsTranslateX: string;
+  setDetailsTranslateX: Function;
+}> = ({ detailsTranslateX, setDetailsTranslateX }) => {
+  const [years, setYears] = useState<number>(new Date().getFullYear());
+  const [months, setMonths] = useState<number>(new Date().getMonth() + 1);
+  const [isDropdown, setIsDropdown] = useState<boolean>(false);
+  const defaultMonth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  const handleDropDown = () => {
+    setIsDropdown(!isDropdown);
+  };
+  return (
+    <>
+      <div className={details.space}></div>
+      <Link to=".." className={details.back}>
+        <i className="fa-solid fa-chevron-left"></i>
+      </Link>
+      <div className={details.navbarLayout}>
+        <div
+          className={details.triggerLink}
+          style={{ transform: detailsTranslateX }}
+        ></div>
+        <div className={details.navbarContainer}>
+          <Link
+            onClick={() => setDetailsTranslateX("translateX(-102.5%)")}
+            to="pay"
+          >
+            支出
+          </Link>
+          <Link
+            onClick={() => setDetailsTranslateX("translateX(0)")}
+            to="income"
+          >
+            收入
+          </Link>
+          <Link
+            onClick={() => setDetailsTranslateX("translateX(102.5%)")}
+            to="remainder"
+          >
+            結餘
+          </Link>
+        </div>
+      </div>
+      <div className={home.header}>
+        <div className={home.dropdownTitle} onClick={handleDropDown}>
+          <div>
+            {years}年{months}月
+          </div>
+          <div
+            className={home.dropdown}
+            style={isDropdown ? { transform: "rotate(180deg)" } : {}}
+          >
+            <i className="fa-solid fa-caret-up"></i>
+          </div>
+        </div>
+      </div>
+      <div
+        onClick={handleDropDown}
+        className={isDropdown ? home.dropdownLayout : ""}
+      ></div>
+      <div
+        className={home.dropdownList}
+        style={isDropdown ? { transform: "translateY(50px)" } : {}}
+      >
+        <div className={home.selectYear}>
+          <div onClick={() => setYears((prev) => prev - 1)}>
+            <i className="fa-solid fa-caret-left"></i>
+          </div>
+          <p>{years}</p>
+          <div onClick={() => setYears((prev) => prev + 1)}>
+            <i className="fa-solid fa-caret-right"></i>
+          </div>
+        </div>
+        <div className={home.selectMonth}>
+          {defaultMonth.map((month) => (
+            <div onClick={() => setMonths(month)} key={month}>
+              {month}月
             </div>
-            <Outlet />
-        </>
-    )
-}
+          ))}
+        </div>
+      </div>
+      <Outlet context={{ years } satisfies ContextType} />
+    </>
+  );
+};
 
-export default Details
+export default Details;
