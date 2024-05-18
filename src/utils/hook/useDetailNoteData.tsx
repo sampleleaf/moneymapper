beb50dc1033782854042a84d7ba29fe66883fb0d
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-05-18 18:05:34 +0800

    refactor: firebase function of useDetailNoteData

--- src/utils/hook/useDetailNoteData.tsx
+++ src/utils/hook/useDetailNoteData.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-import { db } from "@/utils/firebase";
-import { doc, getDoc } from "firebase/firestore";
 import { Item } from "@/interfaces";
+import { getFireStore } from "../reviseFireStore";
 
 const useDetailNoteData = (
   years: number,
@@ -17,16 +16,15 @@ const useDetailNoteData = (
     if (response !== null && popItem) {
       const data = JSON.parse(response);
       (async () => {
-        const docRef = doc(db, "users", data.id, `${years}`, `${months}`);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          const allItemsOfDay: Item[] = docSnap.data()[day];
-          const itemFilter = allItemsOfDay.filter(
-            (allItems) => allItems.item === popItem &&
-              ((!isPositive && allItems.price < 0) || (isPositive && allItems.price > 0)) //(pay || income)
-          );
-          setItems(itemFilter);
-        }
+        const itemsOfMonth = await getFireStore("users", data.id, years, months);
+        const allItemsOfDay: Item[] = itemsOfMonth[day];
+        const itemFilter = allItemsOfDay.filter(
+          (allItems) =>
+            allItems.item === popItem &&
+            ((!isPositive && allItems.price < 0) ||  //pay
+              (isPositive && allItems.price > 0)) //income
+        );
+        setItems(itemFilter);
       })();
     }
   }, []);
